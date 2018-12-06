cc27f8f7b645e39a4f5463ff8dcce6a2e4a5c8cf
Ash Tappin <ashtappin1592@hotmail.com>
2018-12-06 12:31:48 +0000

    Ensure results are emptied when there is no query

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { BandSearchService } from '../bandSearch.service';
-import { debounceTime, defaultIfEmpty, distinctUntilChanged, filter, switchMap } from 'rxjs/internal/operators';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/internal/operators';
+import { EMPTY } from 'rxjs/index';
 
 @Component({
   selector: 'app-search-bar',
@@ -19,10 +20,16 @@ export class SearchBarComponent implements OnInit {
 
   ngOnInit() {
     this.queryField.valueChanges
-      //.pipe(debounceTime(200))
+      .pipe(debounceTime(200))
       .pipe(distinctUntilChanged())
-      .pipe(filter(query => query))
-      .pipe(switchMap((query) => this.bandSearchService.search(query)))
+      .pipe(switchMap((query) => {
+        if (query) {
+          return this.bandSearchService.search(query)
+        } else {
+          this.results = [];
+          return EMPTY;
+        }
+      }))
       .subscribe(response => this.results = (response as any).artists.items)
   }
 
