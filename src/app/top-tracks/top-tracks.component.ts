af419684a5911c7fca732347932c660177b8d17e
Ash Tappin <ashtappin1592@hotmail.com>
2018-12-12 16:15:22 +0000

    Adding in graphs

--- src/app/top-tracks/top-tracks.component.ts
+++ src/app/top-tracks/top-tracks.component.ts
@@ -3,6 +3,8 @@ import { BandService } from '../band-service.service';
 import { ActivatedRoute } from '@angular/router';
 import { TopTrackService } from '../top-track-service.service';
 import { AudioFeaturesService } from '../audio-features.service';
+import * as CanvasJS from './canvasjs.min.js';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-top-tracks',
@@ -14,6 +16,9 @@ export class TopTracksComponent implements OnInit {
   bandId: string;
   bandImageLink: string;
   topTracks: any[] = [];
+  danceabilityDataPoints: any[] =[];
+  speechinessDataPoints: any[] =[];
+  valenceDataPoints: any[] =[];
 
   constructor(private bandService: BandService,
               private route: ActivatedRoute,
@@ -22,6 +27,8 @@ export class TopTracksComponent implements OnInit {
   }
 
   ngOnInit() {
+
+
 
     this.route.params.subscribe(params => {
       this.bandId = params['bandid'];
@@ -32,12 +39,13 @@ export class TopTracksComponent implements OnInit {
         this.topTracksService.getTopTracks(this.bandId)
           .subscribe(results => {
             this.topTracks = results['tracks'];
-            this.audioFeaturesService.getAudioFeaturesForTracks(this.topTracks.map(track => track['id']))
+            this.audioFeaturesService.getAudioFeaturesForTracks(this.topTracks
+              .map(track => track['id']))
               .subscribe(results => {
                 this.topTracks = this.topTracks
                   .map(track => {
                     const trackAudioFeatures = results['audio_features'].find((audio) => audio.id === track.id);
-                    return {...trackAudioFeatures, ...track}
+                    return {...trackAudioFeatures, ...track, date: moment(track.album.release_date, 'YYYY-MM-DD').toDate() }
                   }).sort((last, next) => {
                     if (last.album.release_date < next.album.release_date) {
                       return -1;
@@ -45,10 +53,48 @@ export class TopTracksComponent implements OnInit {
                     if (last.album.release_date > next.album.release_date) {
                       return 1;
                     }
-
-                    // names must be equal
                     return 0;
-                  })
+                  });
+
+                this.danceabilityDataPoints = this.topTracks.map(track => {return {x: track.date, y : track.danceability, name: track.name, toolTipContent: "{name}: {x}", cursor: "pointer"}})
+                this.speechinessDataPoints = this.topTracks.map(track => {return {x: track.date, y : track.speechiness, name: track.name, toolTipContent: "{name}: {x}", cursor: "pointer"}})
+                this.valenceDataPoints = this.topTracks.map(track => {return {x: track.date, y : track.valence, name: track.name, toolTipContent: "{name}: {x}", cursor: "pointer"}})
+
+                let chart = new CanvasJS.Chart("chartContainer", {
+                  theme: 'dark1',
+                  animationDuration: 3000,
+                  animationEnabled: true,
+                  axisX: {
+                    valueFormatString: "MMM YYYY"
+                  },
+                  legend: {
+                    cursor: "pointer",
+                    verticalAlign: "top",
+                    horizontalAlign: "center",
+                    dockInsidePlotArea: true,
+                  },
+                  data: [{
+                    type: "line",
+                    name: "danceability",
+                    showInLegend: true,
+                    dataPoints: this.danceabilityDataPoints
+                  },{
+                    type: "line",
+                    name: "speechiness",
+                    showInLegend: true,
+                    dataPoints: this.speechinessDataPoints
+                  },
+                    {
+                      type: "line",
+                      name: "valence",
+                      showInLegend: true,
+                      dataPoints: this.valenceDataPoints
+                    }
+                  ]
+                });
+
+                chart.render();
+
               })
           });
       });
